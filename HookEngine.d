6e88b56f168e79318ac40684fbf284cd22ee1dd1
GottfriedLehona <GottfriedLehona@2e52861e-7267-437c-9073-90786e7a2ebf>
2012-03-17 22:19:15 +0000

    HookEngine kann jetzt mehrfach an der selben Stelle hooken
    git-svn-id: file:///home/lehona/Downloads/lego_svn/lego@13 2e52861e-7267-437c-9073-90786e7a2ebf

--- HookEngine.d
+++ HookEngine.d
@@ -3,14 +3,6 @@
 \***********************************/
 
 
-//========================================
-// Enginefunktion beenden
-//========================================
-const int _EngineStackSize = -1; // INTERN!
-func void SkipEngineFunction(var int stack) {
-    _EngineStackSize = stack;
-};
-
 //-------------------
 // OPCODES
 //-------------------
@@ -22,60 +14,20 @@ const int ASMINT_OP_popEAX      = 88;    //0x58
 const int ASMINT_OP_addMemToESP = 9475;  //0x2503
 
 //-------------------
-// RÜCKGABEVARIABLEN
+// Registervariablen
 //-------------------
 var int EAX;
-func int GetEAX() { return EAX; };
-func int EAXAdr() {
-    GetEAX();
-    MEMINT_StackPopInst();
-    MEMINT_StackPushInst(zPAR_TOK_PUSHINT);
-};
 var int ECX;
-func int GetECX() { return ECX; };
-func int ECXAdr() {
-    GetECX();
-    MEMINT_StackPopInst();
-    MEMINT_StackPushInst(zPAR_TOK_PUSHINT);
-};
 var int ESP;
-func int GetESP() { return ESP; };
-func int ESPAdr() {
-    GetESP();
-    MEMINT_StackPopInst();
-    MEMINT_StackPushInst(zPAR_TOK_PUSHINT);
-};
-
 var int EBX;
-func int GetEBX() { return EBX; };
-func int EBXAdr() {
-    GetEBX();
-    MEMINT_StackPopInst();
-    MEMINT_StackPushInst(zPAR_TOK_PUSHINT);
-};
-
 var int EBP;
-func int GetEBP() { return EBP; };
-func int EBPAdr() {
-    GetEBP();
-    MEMINT_StackPopInst();
-    MEMINT_StackPushInst(zPAR_TOK_PUSHINT);
-};
-
 var int EDI;
-func int GetEDI() { return EDI; };
-func int EDIAdr() {
-    GetEDI();
-    MEMINT_StackPopInst();
-    MEMINT_StackPushInst(zPAR_TOK_PUSHINT);
-};
 
 
 //========================================
 // Engine hooken
 //========================================
-func void HookEngine(var int address, var int oldInstr, var string function) {
-    function = STR_Upper(function);
+func void HookEngineI(var int address, var int oldInstr, var int function) { 
 
     var int SymbID;   // Symbolindex von 'function'
     var int ptr;      // Pointer auf den Zwischenspeicher der alten Anweisung
@@ -87,21 +39,26 @@ func void HookEngine(var int address, var int oldInstr, var string function) {
         return;
     };
 
-    SymbID = MEM_FindParserSymbol(function);
-    if(!SymbID) {
+    SymbID = function;
+    if(SymbID == -1) {
         PrintDebug("HOOKENGINE: Die gegebene Daedalusfunktion kann nicht gefunden werden.");
         return;
     };
 
     MemoryProtectionOverride (address, oldInstr+3);
     // ----- Eventuell geschützen Speicher behandeln -----
+	
+	if (MEM_ReadByte(address) == 233) { // Hook schon vorhanden
+		HookEngineI(MEM_ReadInt(address+1)+address+5+81, oldInstr, function); 
+		return;
+	};
 
     // ----- Die alte Anweisung sichern -----
     ptr = MEM_Alloc(oldInstr);
     MEM_CopyBytes(address, ptr, oldInstr);
 
     // ----- Einen neuen Stream für den Assemblercode anlegen -----
-    ASM_Open(140 + oldInstr); // Play it safe.
+    ASM_Open(200 + oldInstr); // Play it safe.
 
     // ----- Jump aus der Enginefunktion in den neuen Code einfügen -----
     relAdr = ASMINT_CurrRun-address-5;
@@ -114,35 +71,35 @@ func void HookEngine(var int address, var int oldInstr, var string function) {
 
     // EAX in Daedalus Variable sichern
     ASM_2(ASMINT_OP_movEAXToMem);
-    ASM_4(EAXAdr());
+    ASM_4(_@(EAX));
     ASM_1(ASMINT_OP_pusha);
 
     // ECX in Daedalus Variable sichern
     ASM_2(ASMINT_OP_movECXtoEAX);
     ASM_2(ASMINT_OP_movEAXToMem);
-    ASM_4(ECXAdr());
+    ASM_4(_@(ECX));
 
     // ESP in Daedalus Variable sichern
     ASM_2(ASMINT_OP_movESPtoEAX);
     ASM_2(ASMINT_OP_addImToEAX);
     ASM_1(4*8);                  // Wegen pushad [Danke an Sektenspinner]
     ASM_2(ASMINT_OP_movEAXToMem);
-    ASM_4(ESPAdr());
+    ASM_4(_@(ESP));
 
     // EBX in Daedalus Variable sichern
     ASM_2(ASMINT_OP_movEBXtoEAX);
     ASM_2(ASMINT_OP_movEAXtoMem);
-    ASM_4(EBXAdr());
+    ASM_4(_@(EBX));
 
     // EBP in Daedalus Variable sichern
     ASM_2(ASMINT_OP_movEBPtoEAX);
     ASM_2(ASMINT_OP_movEAXtoMem);
-    ASM_4(EBPAdr());
+    ASM_4(_@(EBP));
 
     // EDI in Daedalus Variable sichern
     ASM_2(ASMINT_OP_movEDItoEAX);
     ASM_2(ASMINT_OP_movEAXtoMem);
-    ASM_4(EDIAdr());
+    ASM_4(_@(EDI));
 
     // --- Daedalusfunktion aufrufen ---
 
@@ -161,43 +118,12 @@ func void HookEngine(var int address, var int oldInstr, var string function) {
     ASM_1(ASMINT_OP_popa);
 
     ASM_1(ASMINT_OP_movMemToEAX);
-    ASM_4(ECXAdr());
+    ASM_4(_@(ECX));
     ASM_2(ASMINT_OP_movEAXtoECX);
 
     ASM_1(ASMINT_OP_movMemToEAX);
-    ASM_4(EAXAdr());
+    ASM_4(_@(EAX));
 
-
-    ASM_1(ASMINT_OP_pushEAX);
-
-    ASM_1(ASMINT_OP_movMemToEAX);
-    ASM_4(MEM_GetIntAddress(_EngineStackSize));
-
-    ASM_1(ASMINT_OP_CompareEAX);
-    ASM_4(-1);
-
-    ASM_1(ASMINT_OP_JZ_Byte);
-    ASM_1(19);
-
-    // if (_EngineStackSize != -1) {
-
-        const int m1 = -1;
-        ASM_1(ASMINT_OP_movMemToEAX);
-        ASM_4(MEM_GetIntAddress(m1));
-
-        ASM_2(ASMINT_OP_movEAXToMem);
-        ASM_4(MEM_GetIntAddress(_EngineStackSize));
-
-        ASM_1(ASMINT_OP_popEAX);
-
-        ASM_2(ASMINT_OP_addMemToESP);
-        ASM_4(MEM_GetIntAddress(_EngineStackSize));
-
-        ASM_1(ASMINT_OP_retn);
-
-    // }
-
-    ASM_1(ASMINT_OP_popEAX);
 
     // Alte Anweisung wieder einfügen
     MEM_CopyBytes(ptr, ASMINT_Cursor, oldInstr);
@@ -210,5 +136,12 @@ func void HookEngine(var int address, var int oldInstr, var string function) {
     ASM_4(address + oldInstr);
     ASM_1(ASMINT_OP_retn);
 
-    ASM_Close();
+    var int i; i = ASM_Close();
+};
+
+func void HookEngineF(var int address, var int oldInstr, var func function) {
+	HookEngineI(address, oldInstr, MEM_GetFuncID(function));
+};
+func void HookEngine(var int address, var int oldInstr, var string function) {
+    HookEngineI(address, oldInstr, MEM_FindParserSymbol(STR_Upper(function)));
 };
