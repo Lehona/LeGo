d71bc1fe044e8156bc7e447f1076fa719fd85b92
GottfriedLehona <GottfriedLehona@2e52861e-7267-437c-9073-90786e7a2ebf>
2013-05-21 23:25:55 +0000

    First introduction of hashtables, roughly tested.
    git-svn-id: file:///home/lehona/Downloads/lego_svn/lego@60 2e52861e-7267-437c-9073-90786e7a2ebf

--- _Hashtable.d
+++ _Hashtable.d
@@ -4,19 +4,28 @@ class _HT_Obj {
 	var int key;
 	var int val;
 };
-const int HT_SIZE = 49999; // Prim*4! Yay!
+const int HT_SIZE = 599; // Primzahl
+
+
+
+func int _HT_CreatePtr(var int size) {
+	var int ptr; ptr = MEM_ArrayCreate();
+	var zCArray arr; arr = _^(ptr);
+	arr.array = MEM_Alloc(size*4);
+	arr.numAlloc = size*4;
+	arr.numInArray = 0;
+	return +ptr;
+};
 
 func int _HT_Create() {
-	var zCArray arr; arr = _^(MEM_ArrayCreate());
-	arr.array = MEM_Alloc(HT_SIZE*4);
-	arr.numAlloc = HT_SIZE*4;
-	arr.numInArray = 0;
-	return _@(arr)+0;
+	return +_HT_CreatePtr(HT_SIZE);
 };
 
 func int hash(var int val) {
-	return MEM_GetBufferCRC32(_@(val), 4);
+	var int hash; hash = MEM_GetBufferCRC32(_@(val), 4);
+	return hash & 2147483647; // No negative values
 };
+
 
 func void _HT_Insert(var int ptr, var int val, var int key) {
 	var zCArray arr; arr = _^(ptr);
@@ -28,7 +37,38 @@ func void _HT_Insert(var int ptr, var int val, var int key) {
 	};
 	MEM_ArrayInsert(bucket, key);
 	MEM_ArrayInsert(bucket, val);
+	arr.numInArray += 1;
+	/* resize if needed */
+	if (arr.numInArray == arr.numAlloc) {
+		var int am; am = arr.numInArray*2;
+		ptr;
+		am;
+		MEM_Call(_HT_Resize);
+	};
 };
+
+
+func void _HT_Resize(var int ptr, var int size) {
+	var zCArray arr; arr = _^(ptr); var zCArray buck;
+	var int htbl; htbl = _HT_CreatePtr(size); var zCArray hArr; hArr = _^(htbl);
+	var int i; var int j; var int bucket; i = 0; j = 0;
+	repeat(i, arr.numAlloc);
+		bucket = MEM_ReadIntArray(arr.array, i);
+		if (bucket) {
+			buck = _^(bucket);
+			repeat(j, buck.numInArray/2);
+				_HT_Insert(htbl, MEM_ReadIntArray(buck.array, 2*j+1), MEM_ReadIntArray(buck.array, 2*j));
+			end;
+			MEM_Free(bucket);
+		};
+	end;
+	MEM_Free(arr.array);
+	arr.array = hArr.array;
+	arr.numAlloc = size*4;
+	arr.numInArray = hArr.array;
+};
+		
+		
 
 func int _HT_GetValue(var int ptr, var int key) {
 	var zCArray arr; arr = _^(ptr);
@@ -47,6 +87,23 @@ func int _HT_GetValue(var int ptr, var int key) {
 	return -1;
 };
 
+func void _HT_Remove(var int ptr, var int key) {
+	var zCArray arr; arr = _^(ptr);
+	var int h; h = hash(key) % (arr.numAlloc/4);
+	var int bucket; bucket = MEM_ReadIntArray(arr.array, h);
+	if (!bucket) { MEM_Info("HT: Key not found"); return; };
+	var zCArray buck; buck = _^(bucket);
+	var int i;
+	repeat(i, buck.numInArray/2);
+		if (MEM_ArrayRead(bucket, i*2) == key) {
+			MEM_ArrayRemoveIndex(bucket, i*2+1);
+			MEM_ArrayRemoveIndex(bucket, i*2);
+			return;
+		};
+	end;
+	MEM_Info("HT: Key not found"); 
+};
+	
 func void MEM_SetUseInstance(var int inst) {
 	var int ptr; ptr = MEM_ReadIntArray (currSymbolTableAddress, inst);
 	MemoryProtectionOverride(11232304, 10);
