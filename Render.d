c31d879a0d564c21050010da70d98a4748871dc0
GottfriedLehona <GottfriedLehona@2e52861e-7267-437c-9073-90786e7a2ebf>
2013-11-15 13:13:27 +0000

    This file is used for rendering Items and Views onto the screen semi-manually.
    git-svn-id: file:///home/lehona/Downloads/lego_svn/lego@74 2e52861e-7267-437c-9073-90786e7a2ebf

---
+++ Render.d
@@ -0,0 +1,137 @@
+const int _render_wld = 0;
+var int _render_list;
+
+class RenderItem {
+	var int inst;
+	var int itmPtr; // oCItem*
+	var int view;
+	var int view_open;
+	var int priority; // standardmäßig 0!
+};
+instance RenderItem@(RenderItem);
+
+
+func int Render_AddItemPrio(var int itemInst, var int x1, var int y1, var int x2, var int y2, var int priority) {
+	var int h; h = new(RenderItem@);
+	var RenderItem itm; itm = get(h);
+	itm.inst = itemInst;
+	itm.view = View_Create(x1, y1, x2, y2);
+	itm.priority = priority;
+	View_Open(itm.view);
+	MEMINT_GetMemHelper(); // Ich sichere mir jetzt Items im MEM_Helper von Ikarus - gute Idee???
+	CreateInvItem(MEM_Helper, itemInst);
+	itm.itmPtr = _@(item);
+	var zCList l; l = get(_render_list);
+	if (l.data) {
+		List_InsertSorted(getPtr(_render_list), h, _Render_Comparator);
+	} else {
+		l.data = h;
+	};
+	return +h;
+};
+
+func int Render_AddItem(var int itemInst, var int x1, var int y1, var int x2, var int y2) {
+	return +Render_AddItemPrio(itemInst, x1, y1, x2, y2, 0);
+};
+
+func int Render_AddViewPrio(var int view, var int priority) {
+	var int h; h = new(RenderItem@);
+	var RenderItem itm; itm = get(h);
+	itm.inst = 0;
+	itm.view = view;
+	itm.priority = priority;
+	itm.view_open = 1;
+	itm.itmPtr = 0;
+	var zCList l; l = get(_render_list);
+	if (l.data) {
+		List_InsertSorted(getPtr(_render_list), h, _Render_Comparator);
+	} else {
+		l.data = h;
+	};
+	return +h;
+};
+
+func int Render_AddView(var int view) {	
+	return +Render_AddViewPrio(view, 0);
+};
+
+func void Render_OpenView(var int ID) {
+	var RenderItem itm; itm = get(ID);
+	itm.view_open = 1;
+	/* Item-Views werden nur gerendert, wenn sie auch offen sind. */
+	if (itm.itmPtr) {
+		View_Open(itm.view);
+	};
+};
+
+func void Render_CloseView(var int ID) {
+	var RenderItem itm; itm = get(ID);
+	itm.view_open = 0;
+	/* Item-Views werden nur gerendert, wenn sie auch offen sind. */
+	if (itm.itmPtr) {
+		View_Close(itm.view);
+	};
+};
+
+func void Render_Remove(var int ID) {
+	var RenderItem itm; itm = get(ID);
+	if (itm.view) {
+		View_Delete(itm.view);
+	};
+	MEMINT_GetMemHelper();
+	Npc_RemoveInvItem(MEM_Helper, itm.inst); // Alle Gegenstände, die der MEM_Helper hat, werden auch gerade gebraucht.
+	List_Delete(getPtr(_render_list), List_Contains(getPtr(_render_list), ID));
+	delete(ID);
+};
+
+
+func void _Render_Hook_Sub(var int list) {
+	var RenderItem itm;
+	var zCList l; l = _^(list);
+	if (l.data) {
+		itm = get(l.data);
+		if (itm.itmPtr) {
+			oCItem_Render(itm.itmPtr, _render_wld, View_GetPtr(itm.view), floatNULL);
+		} else if ((itm.view_open) && (itm.view)) {
+			View_Render(itm.view);
+		};
+	};
+};
+
+// TODO: Neues Spiel -> Neues Spiel crasht noch.
+func void _Render_Hook() {
+	if (!(getPtr(_render_list))) { return; };
+	List_For(getPtr(_render_list), "_RENDER_HOOK_SUB");
+};
+
+func int _Render_Comparator(var int data1, var int data2) {
+	var RenderItem itm1; itm1 = get(data1);
+	var RenderItem itm2; itm2 = get(data2);
+	return (itm1.priority > itm2.priority);
+};
+
+func void _Render_RestorePointer_Sub(var int list) {
+	var RenderItem itm; 
+	var zCList l; l = _^(list);
+	if (l.data) {
+		itm = get(l.data);
+		if (itm.inst) {
+			if (Npc_HasItems(MEM_Helper, itm.inst)) {
+				Npc_RemoveInvItems(MEM_Helper, itm.inst, Npc_HasItems(MEM_Helper, itm.inst));
+			};
+			CreateInvItem(MEM_Helper, itm.inst);
+			itm.itmPtr = _@(item);
+		};
+	};
+};
+
+func void _Render_RestorePointer() {
+	MEMINT_GetMemHelper();
+	List_For(getPtr(_render_list), "_RENDER_RESTOREPOINTER_SUB");
+};
+
+func void _Render_RestorePointer_Listener(var int state) {
+	if (state == GAMESTATE_SAVING) {
+		_Render_RestorePointer();
+	};
+};
